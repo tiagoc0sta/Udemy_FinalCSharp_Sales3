418b04de3c13063c9543cd0843b0fadd08ef9326
Tiago Costa <tiagodpv@gmail.com>
2021-11-21 13:48:27 -0300

    CRUD Scaffolding

---
+++ Udemy_FinalCSharp_Sales3/Data/Udemy_FinalCSharp_Sales3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Udemy_FinalCSharp_Sales3.Models;
+
+namespace Udemy_FinalCSharp_Sales3.Data
+{
+    public class Udemy_FinalCSharp_Sales3Context : DbContext
+    {
+        public Udemy_FinalCSharp_Sales3Context (DbContextOptions<Udemy_FinalCSharp_Sales3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Udemy_FinalCSharp_Sales3.Models.Department> Department { get; set; }
+    }
+}
--- Udemy_FinalCSharp_Sales3/Startup.cs
+++ Udemy_FinalCSharp_Sales3/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Udemy_FinalCSharp_Sales3.Data;
 
 namespace Udemy_FinalCSharp_Sales3
 {
@@ -33,6 +35,9 @@ namespace Udemy_FinalCSharp_Sales3
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Udemy_FinalCSharp_Sales3Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Udemy_FinalCSharp_Sales3Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

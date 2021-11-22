dcea5125d3bb1fdb4ce05c31190e58f1fe8c3a22
Tiago Costa <tiagodpv@gmail.com>
2021-11-22 20:30:20 -0300

    Tabela cadastro

--- Udemy_FinalCSharp_Sales3/Controllers/SellersController.cs
+++ Udemy_FinalCSharp_Sales3/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Udemy_FinalCSharp_Sales3.Models;
 using Udemy_FinalCSharp_Sales3.Services;
 
 namespace Udemy_FinalCSharp_Sales3.Controllers
@@ -21,5 +22,19 @@ namespace Udemy_FinalCSharp_Sales3.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- Udemy_FinalCSharp_Sales3/Services/SellerService.cs
+++ Udemy_FinalCSharp_Sales3/Services/SellerService.cs
@@ -20,6 +20,12 @@ namespace Udemy_FinalCSharp_Sales3.Services
         {
             return _context.Seller.ToList();
         }  
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     
     }
 }

1e0abb0a6d90731615987472eb3f602f60ee609d
Tiago Costa <tiagodpv@gmail.com>
2021-11-22 23:05:05 -0300

    Tela detalhes e eager loading

--- Udemy_FinalCSharp_Sales3/Services/SellerService.cs
+++ Udemy_FinalCSharp_Sales3/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Udemy_FinalCSharp_Sales3.Data;
 using Udemy_FinalCSharp_Sales3.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Udemy_FinalCSharp_Sales3.Services
 {
@@ -29,7 +30,7 @@ namespace Udemy_FinalCSharp_Sales3.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)

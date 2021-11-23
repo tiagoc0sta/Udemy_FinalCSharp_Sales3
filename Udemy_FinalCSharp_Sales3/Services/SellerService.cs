6e078991d0621b3db6cff1e20cfa2c2768a784f1
Tiago Costa <tiagodpv@gmail.com>
2021-11-22 22:48:29 -0300

    Deletar vendedor

--- Udemy_FinalCSharp_Sales3/Services/SellerService.cs
+++ Udemy_FinalCSharp_Sales3/Services/SellerService.cs
@@ -26,6 +26,19 @@ namespace Udemy_FinalCSharp_Sales3.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
     
     }
 }
